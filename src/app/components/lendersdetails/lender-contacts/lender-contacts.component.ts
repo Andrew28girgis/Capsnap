3feb1356a0563c090e2f6455b5f9e93fc3a747c2
Andrew <andrewgirgis28@gmail.com>
2022-06-01 18:08:40 +0200

    last updare

--- src/app/components/lendersdetails/lender-contacts/lender-contacts.component.ts
+++ src/app/components/lendersdetails/lender-contacts/lender-contacts.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Contact } from 'src/app/models/lenders';
+import { LendersService } from 'src/app/services/lenders.service';
 
 @Component({
   selector: 'app-lender-contacts',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lender-contacts.component.css']
 })
 export class LenderContactsComponent implements OnInit {
+  @Input() General:any;
+  constructor(public activatedRoute: ActivatedRoute,private LendersService: LendersService) { }
 
-  constructor() { }
-
+  ContactModel: Contact ;
+  editing:boolean = false;
   ngOnInit(): void {
+    this.ContactModel = new Contact();
+    this.getContacts(this.General.Organization.id);
+
   }
+
+  getContacts(OrgId:any){
+    this.LendersService.OrgContacts(OrgId).subscribe(data => {
+      this.General.Organization.contacts = data;
+    });
+  }
+
+  onSubmit() {
+    this.LendersService.SaveContacts(this.ContactModel).subscribe(data => {
+
+    });
+  }
+
+  closeModal(){
+    this.editing = false;
+    this.ContactModel=  new Contact();
+  }
+
 
 }
